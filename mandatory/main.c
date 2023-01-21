70afaa363db7940cdfa59ee41ffb041e5657bdf9
Achraf Ahrach <aahrach@e2r12p5.1337.ma>
2023-01-21 10:55:23 +0100

    achraf

---
+++ mandatory/main.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahrach <aahrach@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 16:04:02 by aahrach           #+#    #+#             */
+/*   Updated: 2023/01/19 18:50:24 by aahrach          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	func(t_gam *gam)
+{
+	mlx_destroy_window(gam->ml, gam->wi);
+	exit(1);
+}
+
+void	check_dor(t_gam *gam, int key)
+{
+	if ((key == 126) && gam->map[gam->y_p - 1][gam->x_p] == 'E' && !gam->c)
+		print_error(1, NULL, 0);
+	if ((key == 125) && gam->map[gam->y_p + 1][gam->x_p] == 'E' && !gam->c)
+		print_error(1, NULL, 0);
+	if ((key == 124) && gam->map[gam->y_p][gam->x_p + 1] == 'E' && !gam->c)
+		print_error(1, NULL, 0);
+	if ((key == 123) && gam->map[gam->y_p][gam->x_p - 1] == 'E' && !gam->c)
+		print_error(1, NULL, 0);
+}
+
+int	ber(char *p)
+{
+	int	i;
+
+	i = ft_strlen(p);
+	i--;
+	if (p[i] == 'r' && p[i - 1] == 'e' && p[i - 2] == 'b'
+		&& p[i - 3] == '.' && p[i - 4] && p[i - 4] != '/')
+		return (0);
+	return (-1);
+}
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	char	*p;
+	char	**s;
+
+	if (ac != 2 || ber(av[1]) == -1)
+		print_error(0, NULL, 0);
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return (0);
+	p = get_next_line(fd);
+	so_long(p);
+	s = ft_split(p, '\n');
+	game(s);
+	return (0);
+}
---
+++ mandatory/so_long.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahrach <aahrach@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 13:09:54 by aahrach           #+#    #+#             */
+/*   Updated: 2023/01/19 14:48:36 by aahrach          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	print_error(int i, char **map, int x)
+{
+	if (i == 0)
+		write (1, "Error\nargemon is not 2, or *.ber\n", 34);
+	else if (i == -1)
+		write (1, "Error\nkayn new_lin\n", 20);
+	else if (i == -2)
+		write (1, "Error\ncheck_len_map_and_11\n", 28);
+	else if (i == -3)
+		write (1, "Error\nlen dyal stor\n", 21);
+	else if (i == -4)
+		write (1, "Error\nlkadr madayr b 11\n", 25);
+	else if (i == -5)
+		write (1, "Error\ncheck_e_c_p\n", 19);
+	else if (i == -6)
+		write (1, "Error\nnot can P get to E or to all C\n", 38);
+	else if (i == -7)
+		write (1, "Error\nimage !", 14);
+	else if (i == 1)
+		write (1, "rbahti !", 9);
+	if (x == 1)
+		ft_free(map);
+	exit(1);
+}
+
+int	search_p(char **test, int i)
+{
+	int	x;
+	int	y;
+
+	x = 1;
+	y = 1;
+	while (test[y])
+	{
+		x = 1;
+		while (test[y][x])
+		{
+			if (test[y][x] == 'P')
+			{
+				if (i == 0)
+					return (x);
+				return (y);
+			}
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+void	ft_free(char **p)
+{
+	int	i;
+
+	i = 0;
+	if (!p)
+		return ;
+	while (p[i])
+		free(p[i++]);
+	free(p);
+}
+
+void	initialize(t_main *main, int *c_e)
+{
+	c_e[0] = 0;
+	c_e[1] = 0;
+	main->x_p = search_p(main->test, 0);
+	main->y_p = search_p(main->test, 1);
+}
+
+void	so_long(char *p)
+{
+	t_main	main;
+	int		c_e[2];
+
+	main.test = ft_split(p, '\n');
+	if (chek_new_lin(p) == -1)
+		print_error(-1, main.test, 1);
+	main.c = check_len_map_and_11(main.test);
+	if (main.c < 0)
+		print_error(main.c, main.test, 1);
+	initialize(&main, c_e);
+	can_get_point_e(main.test, main.x_p, main.y_p, c_e);
+	if (c_e[1] == 0 || c_e[0] != main.c)
+		print_error(-6, main.test, 1);
+	ft_free(main.test);
+}
